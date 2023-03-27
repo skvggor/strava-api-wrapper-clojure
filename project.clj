ba521eb87200838187ee6b21a2a1e84b71021121
Marcos Lima <marckfree@gmail.com>
2023-03-26 22:24:57 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject strava-api "0.1.0-SNAPSHOT"
-  :description "Simple Strava API"
+  :description "Program written in Clojure that acts as a wrapper for the Strava API."
   :url "http://skvgg.vercel.app"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
